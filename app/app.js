13fabf3ae0b0afbc22e01032269f40f59882a337
opentlc-mgr <ssinelni@redhat.com>
2020-05-29 20:57:49 +0000

    cambio de puerto

--- app/app.js
+++ app/app.js
@@ -19,6 +19,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 3000!');
 });
