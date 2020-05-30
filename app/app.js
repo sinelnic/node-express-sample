99b07886883533fa29e1e403600fabc16f4b41e5
opentlc-mgr <ssinelni@redhat.com>
2020-05-30 20:14:51 +0000

    cambio mensaje

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World versión Sinelnicofffffffff!');
+  res.send('Hello World versión Sinelnicofffffffff con Pipeline!');
 });
 
 app.listen(8080, function () {
